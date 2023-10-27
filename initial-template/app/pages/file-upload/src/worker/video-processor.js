206d5dad0789a519a8822f294aeeaf448ce7f577
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-10-27 15:18:27 -0300

    feat: adds on-demand sending for each frame of the file

--- initial-template/app/pages/file-upload/src/worker/video-processor.js
+++ initial-template/app/pages/file-upload/src/worker/video-processor.js
@@ -8,24 +8,40 @@ export class VideoProcessor {
   constructor({ mp4Demuxer }) {
     this.#mp4Demuxer = mp4Demuxer
   }
-  async mp4Decoder(encoderConfig, stream) {
-    const decoder = new VideoDecoder({
-      output: (frame) => {},
-      error: (error) => console.error('MP4Decoder', error),
-    })
-    await this.#mp4Demuxer.run(stream, {
-      onConfig: (config) => decoder.configure(config),
-      /**
-       * @param {EncodedVideoChunk} chunk 
-       */
-      onChunk: (chunk) => {
-        decoder.decode(chunk) // When processed call the ouput function
-      } // Called when onSamples is executed
+  /**
+   * @returns {ReadableStream}
+   */
+  mp4Decoder(encoderConfig, stream) {
+    return new ReadableStream({
+      start: async (controller) => { // Controller send data
+        const decoder = new VideoDecoder({
+          /**
+           * @param {VideoFrame} frame 
+           */
+          output: (frame) => controller.enqueue(frame),
+          error: (error) => {
+            console.error('MP4Decoder', error)
+            controller.error(error)
+          },
+        })
+        await this.#mp4Demuxer.run(stream, {
+          onConfig: (config) => decoder.configure(config),
+          /**
+           * @param {EncodedVideoChunk} chunk 
+           */
+          onChunk: (chunk) => {
+            decoder.decode(chunk) // When processed call the ouput function
+          } // Called when onSamples is executed
+        })
+      },
     })
   }
   async start({ file, encoderConfig, sendMessage }) {
     const stream = file.stream()
     const fileName = file.name.split('/').pop().replace('.mp4', '')
     await this.mp4Decoder(encoderConfig, stream)
+      .pipeTo(new WritableStream({
+        write: (frame) => console.log(frame)
+      }))
   }
 }
