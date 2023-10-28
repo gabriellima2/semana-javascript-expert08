ff87bbbece558a73e4a3e3910e298c9fb7f9bc7a
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-10-28 13:46:00 -0300

    feat: adds video-decoder config validation

--- initial-template/app/pages/file-upload/src/worker/video-processor.js
+++ initial-template/app/pages/file-upload/src/worker/video-processor.js
@@ -11,7 +11,7 @@ export class VideoProcessor {
   /**
    * @returns {ReadableStream}
    */
-  mp4Decoder(encoderConfig, stream) {
+  mp4Decoder(stream) {
     return new ReadableStream({
       start: async (controller) => { // Controller send data
         const decoder = new VideoDecoder({
@@ -25,7 +25,11 @@ export class VideoProcessor {
           },
         })
         return this.#mp4Demuxer.run(stream, {
-          onConfig: (config) => decoder.configure(config),
+          onConfig: async (config) => {
+            const { supported } = await VideoDecoder.isConfigSupported(config)
+            if (!supported) throw new Error(`Video Decoder config not supported ${config}`)
+            decoder.configure(config)
+          },
           /**
            * @param {EncodedVideoChunk} chunk 
            */
@@ -38,10 +42,11 @@ export class VideoProcessor {
       },
     })
   }
+  encode144p(encodeConfig) {}
   async start({ file, encoderConfig, renderFrame }) {
     const stream = file.stream()
     const fileName = file.name.split('/').pop().replace('.mp4', '')
-    await this.mp4Decoder(encoderConfig, stream)
+    await this.mp4Decoder(stream)
       .pipeTo(new WritableStream({
         write: (frame) => renderFrame(frame),
       }))
